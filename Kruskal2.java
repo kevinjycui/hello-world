8b00aebc0273ac7d10f2b27cee3484fba3e84050
Cui Junyang <38621453+ManchurioX@users.noreply.github.com>
2018-04-22 10:59:56 -0400

    Add files via upload

---
+++ Kruskal2.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Kruskal2 {
+	int V;
+	int E;
+	Edge[] edge;
+	public class Edge implements Comparable<Edge>{
+		int bv;
+		int ev;
+		int cost;
+		@Override
+		public int compareTo(Edge o) {
+			// TODO Auto-generated method stub
+			return this.cost-o.cost;
+		}
+	}
+	public Kruskal2(int v, int e) {
+		V = v;
+		E = e;
+		edge = new Edge[E];
+		for (int i=0; i<E; i++) {
+			edge[i] = new Edge();
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int v = 5;
+		int e = 7;
+		Kruskal2 graph = new Kruskal2(v, e);
+		for (int i=0; i<e; i++) {
+			graph.edge[i].bv = sc.nextInt();
+			graph.edge[i].ev = sc.nextInt();
+			graph.edge[i].cost = sc.nextInt();
+		}
+//		for (int i=0; i<e; i++) {
+//			System.out.print(graph.edge[i].bv+" ");
+//			System.out.print(graph.edge[i].ev+" ");
+//			System.out.print(graph.edge[i].cost+" ");
+//			System.out.println();
+//		}
+		
+	}
+
+}
+
+
